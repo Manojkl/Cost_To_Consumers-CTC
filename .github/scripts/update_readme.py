b46b1c57228f0cf6d5e969848d79578386a69290
Manoj Kolpe <manojmanu517@gmail.com>
2025-05-15 19:22:12 +0530

    Update update_readme.py

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -1,11 +1,31 @@
 from datetime import datetime
 
-# Set your project start date here
+# Road1
 start_date = datetime(2025, 4, 16)
 today = datetime.utcnow()
 days_elapsed = (today - start_date).days
 
 readme_path = "./Road/Readme.md"  # Use the correct relative path
+
+with open(readme_path, "r", encoding="utf-8") as f:
+    content = f.read()
+
+new_content = content.replace(
+    content[
+        content.find("<!--DAYS_COUNTER-->") + 19 : content.find("<!--/DAYS_COUNTER-->")
+    ],
+    str(days_elapsed),
+)
+
+with open(readme_path, "w", encoding="utf-8") as f:
+    f.write(new_content)
+
+# Road2
+start_date = datetime(2025, 5, 15)
+today = datetime.utcnow()
+days_elapsed = (today - start_date).days
+
+readme_path = "./Road/Bangalore/Mahadevapura/Readme.md"  # Use the correct relative path
 
 with open(readme_path, "r", encoding="utf-8") as f:
     content = f.read()
